45283bffe4ad769763c404615bad86b2b41d7fbc
Jenya Chistyakov <97852921+jenyachistyakov@users.noreply.github.com>
2022-10-25 08:19:50 +0300

    Removed extra comments and unused code. The variable and function have a private access level.

--- YP-HomeWork-Sprint3/ViewController.swift
+++ YP-HomeWork-Sprint3/ViewController.swift
@@ -9,15 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    //@IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var counterLabel: UILabel!
-    var count = 0
+    @IBOutlet private weak var counterLabel: UILabel!
+    private var count = 0
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    @IBAction func counterButton(_ sender: Any) {
+    @IBAction private func counterButton(_ sender: Any) {
         count = count + 1
             counterLabel.text = "Значение счётчика:\n\n\(count)"
     }
